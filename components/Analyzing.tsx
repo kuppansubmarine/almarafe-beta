24fd968d2e4fca82cb3fd1bd57706edcc275b397
kuppansubmarine <almarahealth@gmail.com>
2024-07-02 12:53:01 -0400

    Update Analyzing.tsx

--- components/Analyzing.tsx
+++ components/Analyzing.tsx
@@ -10,7 +10,7 @@ const AnalyzingPage = () => {
   return (
     <div className="h-screen bg-gray-50 flex flex-col items-center justify-center">
     
-    <Image className="h-30 w-30 sm:w-40 sm:h-40 mb-3" alt="logo" src={logo}></Image>
+    <Image className="h-20 w-20 sm:w-40 sm:h-40 mb-3" alt="logo" src={logo}></Image>
     <h1 className='mb-1 text-2xl text-gray-500 font-medium'>Analyzing...</h1>
     <p className='mb-10  text-gray-400 font-medium'>Estimated time: 5-10 minutes</p>
 
