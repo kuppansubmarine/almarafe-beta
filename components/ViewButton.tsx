ef573d0429b4d08ffa75194eece5748c771301a3
adhavp <almarahealth@gmail.com>
2024-10-03 18:18:52 -0400

    front-end updates

--- components/ViewButton.tsx
+++ components/ViewButton.tsx
@@ -16,7 +16,7 @@ const ViewButton: React.FC<ViewButtonProps> = ({ NCTID }) => {
 
   return (
     <button
-      className="bg-[#ba0c2f] hover:scale-90 hover:bg-[#70071c] cursor-pointer transition-all duration-200 ease-in-out text-white font-semibold py-2  px-3 text-sm md:text-[15px] rounded-md focus:outline-none"
+      className="bg-[#ba0c2f]  hover:scale-90 hover:bg-[#70071c] cursor-pointer transition-all duration-200 ease-in-out text-white font-semibold py-2 px-3 text-sm md:text-[15px] rounded-md focus:outline-none"
       onClick={handleViewTrial}
     >
       View
